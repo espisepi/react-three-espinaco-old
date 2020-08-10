68a8bfbc5d4be1f5fad42806a1f82b149322cfdd
espisepi <sepinaco@gmail.com>
2020-08-11 00:59:44 +0200

    La barra lateral desaparece -> overflow:hidden !

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Main />
     </div>
   );
--- src/stores.js
+++ src/stores.js
@@ -4,8 +4,14 @@ export const fetchProducts = selector({
     key: 'productsSelector',
     get: async ({get}) => {
         try {
-            const response = await fetch('http://46.101.166.115/wp-json/wp/v2/product');
-            const data = await response.json();
+            // const response = await fetch('http://46.101.166.115/wp-json/wp/v2/product');
+            // const data = await response.json();
+            //products[0].content.rendered
+            const data = [
+                {
+                    content:{rendered: "Hola mundo"},
+                },
+            ];
             return data;
         } catch (error) {
             throw error;
