5f2a9f3cacd3a5a56c54c575191b622710c22fe8
espisepi <sepinaco@gmail.com>
2020-08-11 19:51:48 +0200

    Plane component con movimiento

--- src/components/3d/Plane.jsx
+++ src/components/3d/Plane.jsx
@@ -1,9 +1,13 @@
 import React, { useRef } from "react";
+import { useFrame } from 'react-three-fiber';
 
 const Plane = ({position, rotation}) => {
   position = position || [0, -1, 0];
   rotation = rotation || [Math.PI / 2, 0, 0];
   const plane = useRef();
+  useFrame(({clock})=>{
+    plane.current.position.z = 0.5 * (clock.elapsedTime % 4);
+  });
   return (
     <mesh ref={plane} rotation={rotation} position={position}>
       <planeBufferGeometry attach="geometry" args={[20,20, 20, 20]} />
