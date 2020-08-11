dca51e98c865b199b4db546290e5f453a086f9b1
espisepi <sepinaco@gmail.com>
2020-08-11 19:13:21 +0200

    plane component con position y rotation como props

--- src/components/3d/Plane.jsx
+++ src/components/3d/Plane.jsx
@@ -1,9 +1,11 @@
 import React, { useRef } from "react";
 
-const Plane = () => {
+const Plane = ({position, rotation}) => {
+  position = position || [0, -1, 0];
+  rotation = rotation || [Math.PI / 2, 0, 0];
   const plane = useRef();
   return (
-    <mesh ref={plane} rotation={[Math.PI / 2, 0, 0]} position={[0,-1,0]}>
+    <mesh ref={plane} rotation={rotation} position={position}>
       <planeBufferGeometry attach="geometry" args={[20,20, 20, 20]} />
       <meshStandardMaterial attach="material" color='#ff0000' wireframe={true} />
     </mesh>
