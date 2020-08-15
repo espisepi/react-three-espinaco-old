56406d84ea86fd25e2483a45efcee21c32f292ec
espisepi <sepinaco@gmail.com>
2020-08-15 22:41:12 +0200

    AudioVisualizerShader optimizado con useMemo (ya funciona en todo momento, antes dejaba de funcionar cuando se cambiaba el tamano de la pantalla)

--- src/components/3d/AudioVisualizerShader.jsx
+++ src/components/3d/AudioVisualizerShader.jsx
@@ -3,41 +3,42 @@ import * as THREE from 'three';
 import { useLoader, useFrame } from 'react-three-fiber';
 import { PlaneShaderParams, CubeShaderParams } from './shaders/AudioShaders';
 
-const AudioVisualizerShader = ({audioSrc, mesh, img}) => {
+const AudioVisualizerShader = ({audioSrc, img}) => {
     audioSrc = audioSrc || 'assets/masna.mp3';
     img = img || 'assets/highkili.png';  
 
     const audioBuffer = useLoader(THREE.AudioLoader, audioSrc);
-    const audioListener = new THREE.AudioListener();
-    const audio = new THREE.Audio(audioListener);
-    audio.setBuffer(audioBuffer);
-    audio.setLoop(true);
-    audio.setVolume(0.5);
+    const audioListener = useMemo(() => new THREE.AudioListener(),[]);
+    const audio = useMemo(() => new THREE.Audio(audioListener),[]);
     useMemo(()=>{
+        audio.setBuffer(audioBuffer);
+        audio.setLoop(true);
+        audio.setVolume(0.5);
         audio.play();
     },[]);
     
     const fftSize = 2048;
-    const analyser = new THREE.AudioAnalyser(audio, fftSize);
+    const analyser = useMemo(() => new THREE.AudioAnalyser(audio, fftSize),[]);
 
-    const {vertexShader,fragmentShader,uniforms} = CubeShaderParams(analyser, fftSize);
+    const {vertexShader,fragmentShader,uniforms} = useMemo(()=> CubeShaderParams(analyser, fftSize),[]);
 
     useFrame(()=>{
         analyser.getFrequencyData();
+        
         uniforms.tAudioData.value.needsUpdate = true;
     });
 
+    const mesh = useMemo(()=> new THREE.Mesh(
+        new THREE.PlaneBufferGeometry(2,2,10,10),
+        new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader
+        })
+    ), []);
+
     return (
-        <mesh position={[-3,0,0]} rotation={[-Math.PI / 2, 0 ,0]}>
-            <planeBufferGeometry attach="geometry" args={[2, 2, 10, 10]} />
-            <shaderMaterial 
-                attach="material"
-                uniforms={uniforms}
-                fragmentShader={fragmentShader}
-                vertexShader={vertexShader}
-                 
-            />
-        </mesh>
+        <primitive object={mesh} position={[0,0,0]} />
     );
 };
 
